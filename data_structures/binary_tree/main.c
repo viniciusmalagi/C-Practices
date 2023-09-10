1817b80033a590a657c46a5bce1c3843e74e267b
Vinicius <vinicios.malagi@gmail.com>
2023-09-10 16:13:15 -0300

    Fixes

--- data_structures/binary_tree/main.c
+++ data_structures/binary_tree/main.c
@@ -8,10 +8,11 @@ struct Node {
 };
 
 struct Node* do_create_new_node(int value);
+struct Node* do_remove_current(struct Node* node);
 struct Node* do_append_node(struct Node* node, int value);
 int do_find_node(struct Node* node, int data);
 int do_remove_node(struct Node* node, int data);
-void do_call_node_remover(struct Node* node, int data);
+
 void do_print_in_order(struct Node* node);
 void do_print_pre_order(struct Node* node);
 void do_print_pos_order(struct Node* node);
@@ -29,13 +30,15 @@ int main() {
     my_node = do_append_node(my_node, 62);
     my_node = do_append_node(my_node, 53);
     my_node = do_append_node(my_node, 77);
+    my_node = do_append_node(my_node, 4);
 
     printf("(Before removal) Binary Tree in order: ");
     do_print_in_order(my_node);
     printf("\n");
-    do_call_node_remover(my_node, 7);
+    do_remove_node(my_node, 4);
     printf("Binary Tree in order: ");
     do_print_in_order(my_node);
+    return 0;
     printf("\n");
     printf("Binary Tree pre order: ");
     do_print_pre_order(my_node);
@@ -78,19 +81,60 @@ int do_find_node(struct Node* node, int data){
         return do_find_node(node->left, data);
 }
 
-int do_remove_node(struct Node* node, int data){
-    if (node->data == data && node->left == NULL && node->left == NULL)
-        return 0;
-    else if (data > node->data && !do_remove_node(node->right, data))
-        node->right = NULL;
-    else if (data < node->data && !do_remove_node(node->left, data))
-        node->left = NULL;
-    return 1;
+struct Node* do_remove_current(struct Node* node){
+    struct Node *node_1, *node_2;
+    if (node->left == NULL)
+    //No child on the right.
+    //Point to the child on the left.
+    //Treats leaf node and node with a child.
+    {
+        node_2 = node->right;
+        free(node);
+        return node_2;
+    }
+    //Search for the rightmost child of the left subtree
+    node_1 = node;
+    node_2 = node->left;
+    while (node_1 != node)
+    {
+        node_1 = node_2;
+        node_2 = node_2->right;
+    }
+    //Copies the rightmost child of the left subtree to the location of the node to be removed
+    if (node_1 != node)
+    {
+        node_1->right = node_2->left;
+        node_2->left = node->left;
+    }
+    node_2->right = node->right;
+    free(node);
+    return node_2;
 }
 
-void do_call_node_remover(struct Node* node, int data){
-    if (do_find_node(node, data) && do_remove_node(node, data))
-        printf("\nNode (%d) removed!\n", data);
+int do_remove_node(struct Node* node, int data){
+   if(node == NULL) return 0;
+   struct Node* prev = NULL;
+   struct Node* current = node;
+   while (current != NULL){
+        if (data == current->data){
+            //Found the knot to be removed. Treat the removal
+            if (current == node)
+                node = do_remove_current(current);
+            else{
+                if (prev->right == current)
+                    prev->right = do_remove_current(current);
+                else
+                    prev->left = do_remove_current(current);
+            }
+            return 1;
+        }
+        //Continue walking through the tree looking for the node to be removed
+        prev = current;
+        if (data > current->data)
+            current = current->right;
+        else
+            current = current->left;
+    }
 }
 
 void do_print_in_order(struct Node* node) {
